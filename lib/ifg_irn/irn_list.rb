cc4ec7ae17374c0b90002ea18a4a6bb7b1192e0e
Mourad Hammiche <mourad.hammiche@ifeelgoods.com>
2015-08-17 17:08:25 +0200

    Add Set arithmetics

--- lib/ifg_irn/irn_list.rb
+++ lib/ifg_irn/irn_list.rb
@@ -10,6 +10,11 @@ class IrnList
     @irns = Set.new(irns.map { |x| Irn.new(x) })
   end
 
+  def initialize_dup(source)
+    super
+    @irns = source.instance_variable_get(:@irns).dup
+  end
+
   def <<(irn)
     @irns << Irn.new(irn)
     self
@@ -39,8 +44,30 @@ class IrnList
     restriction
   end
 
+  alias :intersect :restrict
+
   def restrict_with(irn_list)
     irn_list.restrict(self)
+  end
+
+  def subset?(irn_list)
+    assert_valid_type!(irn_list)
+    @irns.all? { |irn| irn_list.member?(irn) }
+  end
+
+  def superset?(irn_list)
+    assert_valid_type!(irn_list)
+    irn_list.all? { |irn| member?(irn) }
+  end
+
+  def union(irn_list)
+    assert_valid_type!(irn_list)
+    dup.merge!(irn_list)
+  end
+
+  def merge!(irn_list)
+    assert_valid_type!(irn_list)
+    self.class.new(@irns.merge(irn_list))
   end
 
   def each
@@ -49,7 +76,12 @@ class IrnList
 
   private
 
+    def assert_valid_type!(object)
+      raise ArgumentError, 'value must be an IrnList' unless object.is_a?(IrnList)
+    end
+
     def inspect_irns
       @irns.map(&:inspect).join(', ')
     end
+
 end
